0a02a4e56f6dd08f920eb4360de58d905c80ee0a
MFBarker <98473836+MFBarker@users.noreply.github.com>
2023-04-06 10:02:12 -0600

    4/6 - catching up and input oak wood assets

---
+++ Assets/Scripts/ControllerCharacter2D.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using UnityEngine;
+
+public class ControllerCharacter2D : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float turnRate;
+    [SerializeField] float jumpHeight;
+    [SerializeField] float doubleJumpHeight;
+    [SerializeField] float hitForce;
+    [SerializeField, Range(1, 5)] float fallRateMultiplier;
+    [SerializeField, Range(1, 5)] float lowJumpRateMultiplier;
+    [Header("Ground")]
+    [SerializeField] Transform groundTransform;
+    [SerializeField] LayerMask groundLayerMask;
+
+    Rigidbody2D rb;
+    Vector2 velocity = Vector3.zero;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        // check if the character is on the ground
+        bool onGround = Physics.CheckSphere(groundTransform.position, 0.02f, groundLayerMask, QueryTriggerInteraction.Ignore);
+        // get direction input
+        Vector2 direction = Vector2.zero;
+        direction.x = Input.GetAxis("Horizontal");
+
+        // set velocity
+        if (onGround)
+        {
+            velocity.x = direction.x * speed;
+            if (velocity.y < 0) velocity.y = 0;
+            if (Input.GetButtonDown("Jump"))
+            {
+                velocity.y += Mathf.Sqrt(jumpHeight * -2 * Physics.gravity.y);
+                StartCoroutine(DoubleJump());
+            }
+        }
+        // adjust gravity for jump
+        float gravityMultiplier = 1;
+        if (!onGround && velocity.y < 0) gravityMultiplier = fallRateMultiplier;
+        if (!onGround && velocity.y > 0 && !Input.GetButton("Jump")) gravityMultiplier = lowJumpRateMultiplier;
+
+        velocity.y += Physics.gravity.y * gravityMultiplier * Time.deltaTime;
+        
+    }
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+        // no rigidbody
+        if (body == null || body.isKinematic)
+        {
+            return;
+        }
+        if (hit.moveDirection.y < -0.3)
+        {
+            return;
+        }
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+        body.velocity = pushDir * hitForce;
+    }
+
+    IEnumerator DoubleJump()
+    {
+        // wait a little after the jump to allow a double jump
+        yield return new WaitForSeconds(0.01f);
+        // allow a double jump while moving up
+        while (velocity.y > 0)
+        {
+            // if "jump" pressed add jump velocity
+            if (Input.GetButtonDown("Jump"))
+            {
+                velocity.y += Mathf.Sqrt(doubleJumpHeight * -2 * Physics.gravity.y);
+                break;
+            }
+            yield return null;
+        }
+    }
+}
