dd6c8bf43d05a772caebb7d8051f68fcdab6627e
MFBarker <98473836+MFBarker@users.noreply.github.com>
2023-04-10 10:50:23 -0600

    4/10 - Enemy, Enemy AI States (update all code to fix other issues in code)

--- Assets/Scripts/AICharacter2D.cs
+++ Assets/Scripts/AICharacter2D.cs
@@ -1,35 +1,86 @@
 using System.Collections;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 //RequireComponent(typeof(Rigidbody2D));
-public class ControllerCharacter2D : MonoBehaviour
+public class AICharacter2D : MonoBehaviour
 {
     [SerializeField] float speed;
     [SerializeField] float jumpHeight;
     [SerializeField] float doubleJumpHeight;
     [SerializeField, Range(1, 5)] float fallRateMultiplier;
     [SerializeField, Range(1, 5)] float lowJumpRateMultiplier;
+
+    [SerializeField] Animator animator;
+    [SerializeField] SpriteRenderer spriteRenderer;
+
     [Header("Ground")]
     [SerializeField] Transform groundTransform;
     [SerializeField] LayerMask groundLayerMask;
     [SerializeField] float groundRadius;
 
-    [SerializeField] Animator animator;
-    [SerializeField] SpriteRenderer spriteRenderer;
+    [Header("AI")]
+    [SerializeField] Transform[] waypoints;
+    [SerializeField] float RayDistance;
 
     Rigidbody2D rb;
     Vector2 velocity = Vector3.zero;
     bool faceRight = true;
+    //float groundAngle = 0;
+    Transform targetWaypoint;
+
+    enum State
+    { 
+        IDLE,
+        PATROL,
+        CHASE,
+        ATTACK
+    }
+
+    State state = State.IDLE;
+    float stateTimer = 0;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
+        Vector2 direction = Vector2.zero;
+
+        //update ai
+        switch (state)
+        {
+            case State.IDLE:
+                if (CanSeePlayer()) state = State.CHASE;
+                stateTimer += Time.deltaTime;
+                if (stateTimer >= 2)
+                {
+                    targetWaypoint = waypoints[Random.Range(0, waypoints.Length)];
+                    state = State.PATROL;
+                }
+                break;
+            case State.PATROL:
+                if (CanSeePlayer()) state = State.CHASE;
+                direction.x = Mathf.Sign(transform.position.x - targetWaypoint.position.x);
+                
+                float dx = Mathf.Abs(transform.position.x - targetWaypoint.position.x);
+                if (dx <= 0.5)
+                {
+                    direction.x = 0;
+                }
+                break;
+            case State.CHASE:
+                break;
+            case State.ATTACK:
+                break;
+
+        }
+
         // check if the character is on the ground
         bool onGround = Physics.CheckSphere(groundTransform.position, 0.02f, groundLayerMask, QueryTriggerInteraction.Ignore);
         // get direction input
-        Vector2 direction = Vector2.zero;
+        
         direction.x = Input.GetAxis("Horizontal");
 
         // set velocity
@@ -99,4 +150,30 @@ public class ControllerCharacter2D : MonoBehaviour
         faceRight = !faceRight;
         spriteRenderer.flipX = faceRight;
     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(groundTransform.position,groundRadius);
+    }
+
+    private void SetNewWayPointTarget()
+    {
+        Transform waypoint= null;
+        do
+        {
+            waypoint = waypoints[Random.Range(0, waypoints.Length)];
+        }
+        while (waypoint == targetWaypoint);
+        targetWaypoint = waypoint;
+    }
+
+    private bool CanSeePlayer()
+    {
+        RaycastHit2D raycasthit = Physics2D.Raycast(transform.position, ((faceRight) ? Vector2.right : Vector2.left) * RayDistance);
+        Debug.DrawRay(transform.position, ((faceRight) ? Vector2.right : Vector2.left) * RayDistance);
+       
+        return raycasthit.collider != null && raycasthit.collider.gameObject.CompareTag("Player");
+    }
+
 }
